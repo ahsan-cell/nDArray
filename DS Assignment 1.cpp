8d9302f6a2306d5158b4be747c556dc69fd536fa
Ahsan Iftikhar <ahsaniftikhar025@gmail.com>
2023-12-08 19:15:57 +0500

    Data structures assignment 1

---
+++ DS Assignment 1.cpp
@@ -0,0 +1,506 @@
+#include<iostream>
+using namespace std;
+#pragma once
+#include <iostream>
+using namespace std;
+
+template <class T>
+class ndArray
+{
+	int* dimensions;
+	int dim_size;
+	T* linearArray;
+	int lin_size;
+public:
+	ndArray();
+	ndArray(int); //for 1D array
+	ndArray(int, int); //for 2D array
+	ndArray(int, int, int);//for 3D array
+	ndArray(int* dimension, int size);
+
+	ndArray(const ndArray& obj);
+
+	bool insert(T element, int i); //insert in 1D array
+	bool insert(T element, int i, int j);//insert in 2D array
+	bool insert(T element, int i, int j, int k);  //insert in 3D array
+	bool insert(T element, int* indexes, int size);
+
+	bool Delete(T const element);     //deletes a particular element and shifts the data
+	bool Delete(int* indexes, int size); // deletes the data at a particular address and shifts the data
+
+	bool Extend();  //doubles the higher dimension
+
+	bool Shrink(); //halves the higher dimension
+
+	T Retrieve(T const element); //Returns the element if not found returns the first element 
+
+	//will return first element when index is invalid
+	T& operator[](const int i);
+	T& operator()(const int i, const int j);
+	T& operator()(const int i, const int j, const int k);
+	T& operator()(const int i, const int j, const int k, const int l);
+
+
+	~ndArray();
+
+	friend ostream& operator<<(ostream& out, const ndArray& obj)
+	{
+		if (obj.dim_size == 2)
+		{
+			int i = 0;
+			for (int j = 0; j < obj.dimensions[0]; j++)
+			{
+				for (int k = 0; k < obj.dimensions[1]; k++)
+				{
+					out << obj.linearArray[i] << " ";
+					i++;
+				}
+				out << endl;
+			}
+		}
+		else if (obj.dim_size == 3)
+		{
+			int i = 0;
+			for (int h = 0; h < obj.dimensions[0]; h++)
+			{
+				for (int j = 0; j < obj.dimensions[1]; j++)
+				{
+					for (int k = 0; k < obj.dimensions[2]; k++)
+					{
+						out << obj.linearArray[i] << " ";
+						i++;
+					}
+					out << endl;
+				}
+				out << endl;
+			}
+		}
+		else
+		{
+			for (int i = 0; i < obj.lin_size; i++)
+				out << obj.linearArray[i] << " ";
+		}
+		return out;
+	}
+
+
+};
+
+
+
+template <class T>
+ndArray<T>::ndArray()
+{
+	dimensions = nullptr;
+	dim_size = 0;
+	linearArray = nullptr;
+	lin_size = 0;
+}
+
+template <class T>
+ndArray<T>::ndArray(int i)
+{
+	dim_size = 1;
+	dimensions = new int[dim_size];
+	dimensions[0] = i;
+
+	lin_size = i;
+	linearArray = new T[lin_size];
+
+	for (int i = 0; i < lin_size; i++)
+		this->linearArray[i] = 0;
+
+}
+
+template <class T>
+ndArray<T>::ndArray(int i, int j)
+{
+	dim_size = 2;
+	dimensions = new int[dim_size];
+	dimensions[0] = i;
+	dimensions[1] = j;
+
+	lin_size = i * j;
+	linearArray = new T[lin_size];
+	for (int i = 0; i < lin_size; i++)
+		this->linearArray[i] = 0;
+
+}
+
+template <class T>
+ndArray<T>::ndArray(int i, int j, int k)
+{
+	dim_size = 3;
+	dimensions = new int[dim_size];
+	dimensions[0] = i;
+	dimensions[1] = j;
+	dimensions[2] = k;
+
+	lin_size = i * j * k;
+	linearArray = new T[lin_size];
+	for (int i = 0; i < lin_size; i++)
+		this->linearArray[i] = 0;
+
+}
+
+template <class T>
+ndArray<T>::ndArray(int* dimension, int size)
+{
+	dim_size = size;
+	dimensions = new int[dim_size];
+	for (int i = 0; i < dim_size; i++)
+		dimensions[i] = dimension[i];
+	lin_size = 1;
+	for (int i = 0; i < dim_size; i++)
+		lin_size = lin_size * dimensions[i];
+
+	linearArray = new T[lin_size];
+	for (int i = 0; i < lin_size; i++)
+		this->linearArray[i] = 0;
+
+
+}
+
+template <class T>
+ndArray<T>::ndArray(const ndArray& obj)
+{
+	this->lin_size = obj.lin_size;
+	this->dim_size = obj.dim_size;
+
+	this->dimensions = new int[dim_size];
+	this->linearArray = new T[lin_size];
+
+	for (int i = 0; i < dim_size; i++)
+		this->dimensions[i] = obj.dimensions[i];
+	for (int i = 0; i < lin_size; i++)
+		this->linearArray[i] = obj.linearArray[i];
+}
+
+
+template <class T>
+bool ndArray<T>::insert(T element, int i)
+{
+	if (dim_size != 1)
+		return false;
+	if (!(i < dimensions[0]))
+		return false;
+	linearArray[i] = element;
+	return true;
+}
+
+template <class T>
+bool ndArray<T>::insert(T element, int i, int j)
+{
+	if (dim_size != 2)
+		return false;
+	if (!(i < dimensions[0] && j < dimensions[1]))
+		return false;
+	int index = (i * dimensions[1]) + j;
+	linearArray[index] = element;
+	return true;
+}
+
+template <class T>
+bool ndArray<T>::insert(T element, int i, int j, int k)
+{
+	if (dim_size != 3)
+		return false;
+	if (!(i < dimensions[0] && j < dimensions[1] && k < dimensions[2]))
+		return false;
+	int index = i * dimensions[1] * dimensions[2] + j * dimensions[2] + k;
+	linearArray[index] = element;
+	return true;
+
+}
+
+
+template <class T>
+bool ndArray<T>::insert(T element, int* indexes, int size)
+{
+	if (dim_size != size)
+	{
+		cout << "Invalid dimension for this objext" << endl;
+		return false;
+	}
+	for (int i = 0; i < dim_size; i++)
+	{
+		if (!(indexes[i] < dimensions[i]))
+		{
+			cout << "Invalid indexes" << endl;
+			return false;
+		}
+	}
+	if (dim_size == 1)
+	{
+		linearArray[indexes[0]] = element;
+		return true;
+	}
+
+	int index = 0;
+
+	for (int i = 0; i < dim_size - 1; i++)
+	{
+		int x = 1;
+		for (int j = i + 1; j < dim_size; j++)
+			x = x * indexes[i] * dimensions[j];
+		index = index + x;
+	}
+	index = index + indexes[size - 1];
+	linearArray[index] = element;
+	return true;
+}
+
+template <class T>
+T& ndArray<T>::operator[](const int i)
+{
+	if (i > 0 && dim_size == 1 && i < dimensions[0])
+	{
+		return linearArray[i];
+	}
+	else
+	{
+		cout << "invalid index ";
+		return linearArray[0];
+
+	}
+}
+
+template <class T>
+T ndArray<T>::Retrieve(T const element)
+{
+	for (int i = 0; i < lin_size; i++)
+		if (linearArray[i] == element)
+			return linearArray[i];
+	cout << "Element not found\n";
+	return linearArray[0];
+}
+
+template<class T>
+bool ndArray<T>::Delete(T const element)
+{
+	int index = -1;
+	for (int i = 0; i < lin_size; i++)
+		if (linearArray[i] == element)
+			index = i;
+	if (index == -1)
+		return false;
+	else
+	{
+		for (int i = index; i < lin_size - 1; i++)
+			linearArray[i] = linearArray[i + 1];
+		linearArray[lin_size - 1] = 0;
+		return true;
+
+	}
+
+
+}
+
+template <class T>
+bool ndArray<T>::Delete(int* indexes, int size)
+{
+	if (dim_size != size)
+	{
+		cout << "Invalid dimension for this objext" << endl;
+		return false;
+	}
+	for (int i = 0; i < dim_size; i++)
+	{
+		if (!(indexes[i] < dimensions[i]))
+		{
+			cout << "Invalid indexes" << endl;
+			return false;
+		}
+	}
+
+	int index = 0;
+	if (dim_size == 1)
+	{
+		index = indexes[0];
+		for (int i = index; i < lin_size - 1; i++)
+			linearArray[i] = linearArray[i + 1];
+		linearArray[lin_size - 1] = 0;
+		return true;
+	}
+
+
+	for (int i = 0; i < dim_size; i++)
+	{
+		for (int j = i + 1; j < dim_size; j++)
+			index = index + indexes[i] * dimensions[j];
+	}
+	index = index + indexes[size - 1];
+
+	for (int i = index; i < lin_size - 1; i++)
+		linearArray[i] = linearArray[i + 1];
+	linearArray[lin_size - 1] = 0;
+	return true;
+
+
+}
+
+template <class T>
+bool ndArray<T>::Extend()
+{
+	if (dim_size == 0 || lin_size == 0)
+		return false;
+	dimensions[0] = dimensions[0] * 2;
+	int newsize = lin_size * 2;
+	T* arr = new T[newsize];
+	for (int i = 0; i < newsize; i++)
+		arr[i] = 0;
+	for (int i = 0; i < lin_size; i++)
+		arr[i] = linearArray[i];
+	delete[]linearArray;
+	linearArray = arr;
+	lin_size = newsize;
+	return true;
+}
+
+template <class T>
+bool ndArray<T>::Shrink()
+{
+	if (dim_size == 0 || dim_size == 1 || lin_size == 0)
+		return false;
+	dimensions[0] = dimensions[0] / 2;
+	lin_size = lin_size / 2;
+	T* arr = new T[lin_size];
+	for (int i = 0; i < lin_size; i++)
+		arr[i] = linearArray[i];
+	delete[] linearArray;
+	linearArray = arr;
+	return true;
+}
+
+template <class T>
+ndArray<T>::~ndArray()
+{
+	delete[] linearArray;
+	delete[] dimensions;
+	linearArray = nullptr;
+	dimensions = nullptr;
+	lin_size = 0;
+	dim_size = 0;
+
+}
+
+template <class T>
+T& ndArray<T>::operator()(const int i, const int j)
+{
+	if (dim_size != 2 || i<0 || j<0 || i>dimensions[0] || j>dimensions[1])
+	{
+		cout << "Invalid index ";
+		return linearArray[0];
+	}
+	int index = i * dimensions[1] + j;
+	return linearArray[index];
+}
+
+template <class T>
+T& ndArray<T>::operator()(const int i, const int j, const int k)
+{
+	if (dim_size != 3 || i<0 || j<0 || k<0 || i>dimensions[0] || j>dimensions[1] || k>dimensions[2])
+	{
+		cout << "Invalid index ";
+		return linearArray[0];
+	}
+	int index = i * dimensions[1] * dimensions[2] + j * dimensions[2] + k;
+	return linearArray[index];
+}
+
+template <class T>
+T& ndArray<T>::operator()(const int i, const int j, const int k, const int l)
+{
+	if (dim_size != 4 || i<0 || j<0 || k<0 || l<0 || i>dimensions[0] || j>dimensions[1] || k>dimensions[2] || l>dimensions[3])
+	{
+		cout << "Invalid operands ";
+		return linearArray[0];
+	}
+	int index = i * dimensions[1] * dimensions[2] * dimensions[3] + j * dimensions[2] * dimensions[3] + k * dimensions[3] + l;
+
+	return linearArray[index];
+
+
+}
+void main()
+{ 
+	//sample 3d array for testing
+	int const size = 3;
+	int arr1[size] = { 2,2,3 };
+	ndArray<int> a1(arr1,size);
+	a1.insert(1, 0, 0, 0);
+	a1.insert(2, 0, 0, 1);
+	a1.insert(3, 0, 0, 2);
+	a1.insert(4, 0, 1, 0);
+	a1.insert(5, 0, 1, 1);
+	a1.insert(6, 0, 1, 2);
+	a1.insert(7, 1, 0, 0);
+	a1.insert(8, 1, 0, 1);
+	a1.insert(9, 1, 0, 2);
+	a1.insert(10, 1, 1, 0);
+	a1.insert(11, 1, 1, 1);
+	a1.insert(12, 1, 1, 2);
+
+	cout << "---------------------------------------\n";
+	cout << a1;                    //testing << operator
+
+	cout << "---------------------------------------\n";
+	cout << a1(1, 1, 10)<<endl;
+	a1(1, 1, 1) = 100;              //testing () operator
+	cout << a1(1, 1, 1)<<endl;
+
+	cout << "---------------------------------------\n";
+	ndArray<int> a2(a1);            //testing copy constructor
+	cout << a2;
+	
+	cout << "---------------------------------------\n";
+	a1.Delete(5);                    //testing delete functions
+	cout << a1;                      
+
+	int arr2[3] = { 0,0,0 };
+	a1.Delete(arr2, 3);
+	cout << a1;
+    
+	cout << "---------------------------------------\n";
+	a1.Extend();                      //testing extend function
+	cout << a1;
+	
+	cout << "---------------------------------------\n";
+	a1.Shrink();                      //testing shrink function
+	cout << a1;
+
+	cout << "---------------------------------------\n";
+	cout << a1.Retrieve(100)<<endl;    //testing retrieve       
+
+	cout << "---------------------------------------\n";
+	//creating a 4d array for testing
+	int arr4[4] = { 2,1,2,2 };
+	int indexes[4] = { 0,0,0,0 };
+	ndArray<int> a3(arr4,4);
+	int x = 1;
+	//using loops to store values at each index
+	for (int i = 0; i <  2; i++)
+	{
+		for (int j = 0; j < 1; j++)
+		{
+			for (int k = 0; k < 2; k++)
+			{
+				for (int l = 0; l < 2; l++)
+				{
+					indexes[0] = i;
+					indexes[1] = j;
+					indexes[2] = k;
+					indexes[3] = l;
+					a3.insert(x, indexes, 4);
+					x++;
+				}
+			}
+		}
+	}
+
+	cout << a3;
+	
+	
+
+	
+}
